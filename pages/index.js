6008553f4fad3aa0a0d882925504c9d72bc9b35b
inoue nobuhide <76238033+nobyn0312@users.noreply.github.com>
2023-04-10 18:50:47 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { client } from '../libs/client'
 import styles from "../styles/Home.module.scss";
-import Topimg from "next/image"; // 1. 画像を読み込むためにnext/imageをインポートする
+import Image from 'next/image';
 
 
 //SSG
@@ -18,8 +18,8 @@ export default function Home({blog}) {
   return (
     <div className={styles.container}>
     <div>
-    <Topimg src="/img/FYA32xmVsAAXyIZ.jpeg" alt="My image" width={640} height={426} />
-    </div>
+    <Image src="/img/FYA32xmVsAAXyIZ.png" alt="My image" width={640} height={426} />
+</div>
       {blog.map((blog) => (
         <li key={blog.id} >
           <Link Link legacyBehavior href={`blog/${blog.id}`}>
