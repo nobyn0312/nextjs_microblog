7b61ce958d7d3c7387592c37bbc3c202eb5a35a0
inoue nobuhide <76238033+nobyn0312@users.noreply.github.com>
2023-04-10 18:07:14 +0900

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import fetch from 'isomorphic-fetch';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch;
 }
