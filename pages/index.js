af4d8e914356dbc36a3c4623d9b493fd7a05f150
inoue nobuhide <76238033+nobyn0312@users.noreply.github.com>
2023-04-10 18:31:18 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { client } from '../libs/client'
 import styles from "../styles/Home.module.scss";
+import Image from  "/img/FYA32xmVsAAXyIZ.jpeg"
 
 //SSG
 export const getStaticProps =async() => {
@@ -15,6 +16,9 @@ export const getStaticProps =async() => {
 export default function Home({blog}) {
   return (
     <div className={styles.container}>
+    <div>
+    <Image src={topimg.src} alt="My image" width={640} height={426} />
+    </div>
       {blog.map((blog) => (
         <li key={blog.id} >
           <Link Link legacyBehavior href={`blog/${blog.id}`}>
